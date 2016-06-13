5990c8ff410df0ffe66ce5354734bae3daceea05
Christos Koutsiaris <koutsiaris@gmail.com>
2016-06-13 14:22:47 +0100

    demo files

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    'twitterAuth' : {
+        'consumerKey'       : 'DQ1xFKaHyZzVd5tz2wFFpc59j',
+        'consumerSecret'    : '8JHGV26T6zwbw2Sd8QfKLUL9xy4a4qR3YrRVCHhxSdEKICzxFf',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    }
+};
